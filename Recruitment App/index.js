feb814f99139892c82617dffdb18b83dd117b5f1
Hitesh Singh <131950172+Hitesh-Singh09@users.noreply.github.com>
2023-07-09 00:07:10 +0530

    Add files via upload

---
+++ Recruitment App/index.js
@@ -0,0 +1,7 @@
+document.getElementById('login-btn').addEventListener('click', function() {
+    window.location.href = 'signin.html';
+  });
+  
+  document.getElementById('search-btn').addEventListener('click', function() {
+    window.location.href = 'signin.html';
+  });
